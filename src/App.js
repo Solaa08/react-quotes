5eeb5e84a5d376652679cd8778f2d3cf5a40da3e
Solaa08 <solarium_08@hotmail.fr>
2022-09-22 18:44:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import './app.css';
 
-function App() {
+// const config = {
+//   headers: {
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+//   }
+// };
+
+const baseURL = "https://api.quotable.io/random";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+  const [save, setSave] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+  
+  // const datas = [
+  //   { author: post.author },
+  //   { content: post.content }
+  // ]
+
+  
+
+  const handleSave = (e) => {
+    console.log(e.target.value)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">
+       <h1>Random Quotes Generator</h1>
+      </div>
+      <div className="content">
+      <h2>{post.author}</h2>
+      <div className="line">
+      <p>{post.content}</p>
+      <button onClick={handleSave} value={post} className="save-button">Save this one</button>
+      </div>
+      </div>
+      <div className="saved-title">
+      <h2>Saved ones</h2>
+      </div>
+      <div className="content">
+      <h2>{post.author}</h2>
+      <div className="line">
+      <p>{post.content}</p>
+      <button className="delete-button">Delete it</button>
+      </div>
+      </div>
+
+
     </div>
   );
 }
-
-export default App;
