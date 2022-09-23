699dda96329dd4b3da08bcf443044581537839c5
Solaa08 <solarium_08@hotmail.fr>
2022-09-23 19:11:56 +0200

    add resfresh onClick

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
 import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 import './app.css';
 
-// const config = {
-//   headers: {
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-//   }
-// };
-
-const baseURL = "https://api.quotable.io/random";
 
 export default function App() {
-  const [post, setPost] = React.useState(null);
-  const [save, setSave] = React.useState(null);
 
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
+  const [post, setPost] = useState([]);
+  const [loadingData, setLoadingData] = useState(true);
+  const [refreshData, setRefreshData] = useState(false);
+  
+  const getData = async ()=>{
+   const response = await axios.get('https://api.quotable.io/random')
+   setPost(response.data)
+   setLoadingData(false);
+  }
+  
+  useEffect(()=>{
+   getData()
+  },[refreshData])
 
   if (!post) return null;
   
-  // const datas = [
-  //   { author: post.author },
-  //   { content: post.content }
-  // ]
-
-  
-
-  const handleSave = (e) => {
-    console.log(e.target.value)
-  }
-
-
 
   return (
     <div>
@@ -45,21 +32,19 @@ export default function App() {
       <h2>{post.author}</h2>
       <div className="line">
       <p>{post.content}</p>
-      <button onClick={handleSave} value={post} className="save-button">Save this one</button>
+      <button onClick={getData} className="save-button">Save this one</button>
       </div>
       </div>
       <div className="saved-title">
       <h2>Saved ones</h2>
       </div>
       <div className="content">
-      <h2>{post.author}</h2>
+      <h2>{}</h2>
       <div className="line">
-      <p>{post.content}</p>
+      <p></p>
       <button className="delete-button">Delete it</button>
       </div>
       </div>
-
-
     </div>
   );
 }
